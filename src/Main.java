0d9a0f4b21f9fceb9fa207ac63c063490e2c5ae7
tekater <120014627+tekater@users.noreply.github.com>
2023-01-19 00:01:12 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,23 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 public class Main {
     public static String readFileAsString(String filename) throws IOException {
         return new String(Files.readAllBytes(Paths.get(filename)));
     }
     public static void main(String[] args) throws IOException {
 
-        String string = new String(Files.readAllBytes(Paths.get("C:\\Users\\user\\Desktop\\text.txt")));
-        //String regex = "[\\w\\s,]";
-        String[] filter = string.split("");
+        String string = new String(Files.readAllBytes(Paths.get("C:\\Users\\text.txt")));  //C:\\Users\\user\\Desktop\\text.txt")));
+        String[] filter = string.split("\\W\\s");
         int len = filter.length;
-
-        System.out.println(len);
-        int i = 1;
-       for () {
-
-            if (len == 0) {
+        int n = 0;
+        for (int i = 0; i < len; i++) {
+            if (Integer.parseInt(filter[i]) == 0) {
                 break;
-            } else if (len > 1) {
-
+            } else if (Integer.parseInt(filter[i]) % 2 == 0) {
+                n++;
             }
-            System.out.println(len); //191
         }
+        System.out.println(n);
     }
 }
